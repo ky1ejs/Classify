4a346b18cfeaee80bac21a7d7ace341d8a7d882c
Kyle McAlpine <kyle.mcalpine@my.westminster.ac.uk>
2015-05-02 14:22:38 +0100

    VisualStudio project created

---
+++ Classify/ViewController.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Classify
+{
+    partial class ViewController
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.homeTabPage = new System.Windows.Forms.TabPage();
+            this.year1TabPage = new System.Windows.Forms.TabPage();
+            this.tabControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.homeTabPage);
+            this.tabControl.Controls.Add(this.year1TabPage);
+            this.tabControl.Location = new System.Drawing.Point(3, 2);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(452, 359);
+            this.tabControl.TabIndex = 0;
+            // 
+            // homeTabPage
+            // 
+            this.homeTabPage.Location = new System.Drawing.Point(4, 22);
+            this.homeTabPage.Name = "homeTabPage";
+            this.homeTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.homeTabPage.Size = new System.Drawing.Size(444, 333);
+            this.homeTabPage.TabIndex = 0;
+            this.homeTabPage.Text = "Home";
+            this.homeTabPage.UseVisualStyleBackColor = true;
+            // 
+            // year1TabPage
+            // 
+            this.year1TabPage.Location = new System.Drawing.Point(4, 22);
+            this.year1TabPage.Name = "year1TabPage";
+            this.year1TabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.year1TabPage.Size = new System.Drawing.Size(444, 333);
+            this.year1TabPage.TabIndex = 1;
+            this.year1TabPage.Text = "Year 1";
+            this.year1TabPage.UseVisualStyleBackColor = true;
+            // 
+            // ViewController
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 362);
+            this.Controls.Add(this.tabControl);
+            this.Name = "ViewController";
+            this.Text = "Calssify";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage homeTabPage;
+        private System.Windows.Forms.TabPage year1TabPage;
+    }
+}
+
---
+++ Classify/ViewController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Classify
+{
+    public partial class ViewController : Form
+    {
+        public ViewController()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.year1TabPage.BackColor = Color.Azure;
+            String dbPath = "ClassifyDB.sqlite";
+            if (!File.Exists(dbPath))
+            {
+                SQLiteConnection.CreateFile("ClassifyDB.sqlite");
+            }
+            SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+            m_dbConnection.Open();
+
+        }
+    }
+}
