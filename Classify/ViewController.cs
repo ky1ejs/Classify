38eb11227243d1402a4bf75df11e0769b9fcb716
Kyle McAlpine <kyle.mcalpine@my.westminster.ac.uk>
2015-05-04 12:47:42 +0100

    Anchors added to views

--- Classify/ViewController.cs
+++ Classify/ViewController.cs
@@ -22,21 +22,40 @@ namespace Classify
         {
             InitializeComponent();
 
+            addModuleButton.Anchor = AnchorStyles.Right | AnchorStyles.Top;
+
             addEditModView = new AddEditModuleView(this);
             addEditModView.Location = new Point(0, 0);
             addEditModView.Size = new Size(this.Size.Width, this.Size.Height);
 
+            tabControl.Anchor = AnchorStyles.Bottom |
+                                AnchorStyles.Right |
+                                AnchorStyles.Top |
+                                AnchorStyles.Left;
+
             year1Table = new ModuleTableView(1);
+            year1Table.Anchor = AnchorStyles.Bottom |
+                                AnchorStyles.Right |
+                                AnchorStyles.Top |
+                                AnchorStyles.Left;
             year1Table.Location = new Point(0, 0);
             year1Table.Size = new Size(year1TabPage.Size.Width, year1TabPage.Size.Height);
             year1TabPage.Controls.Add(year1Table);
 
             year2Table = new ModuleTableView(2);
+            year2Table.Anchor = AnchorStyles.Bottom |
+                                AnchorStyles.Right |
+                                AnchorStyles.Top |
+                                AnchorStyles.Left;
             year2Table.Location = new Point(0, 0);
             year2Table.Size = new Size(year2TabPage.Size.Width, year2TabPage.Size.Height);
             year2TabPage.Controls.Add(year2Table);
 
             year3Table = new ModuleTableView(3);
+            year3Table.Anchor = AnchorStyles.Bottom |
+                                AnchorStyles.Right |
+                                AnchorStyles.Top |
+                                AnchorStyles.Left;
             year3Table.Location = new Point(0, 0);
             year3Table.Size = new Size(year2TabPage.Size.Width, year2TabPage.Size.Height);
             year3TabPage.Controls.Add(year3Table);
